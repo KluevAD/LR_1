a4b78808398802ab8a95cde08e50894dff352a34
Андрей Клюев <fremangrobovshik@mail.ru>
2022-09-19 18:19:19 +0500

    Работа над консольным приложением

--- LR_1_ConsoleApp.cpp
+++ LR_1_ConsoleApp.cpp
@@ -10,23 +10,23 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <LR_1_Group.h>
-#include <LR_1_FIO.h>
-using namespace std
+#include "LR_1_Group.h"
+#include "LR_1_FIO.h"
+using namespace std;
 
- int _tmain(int argc, _TCHAR* argv[]) 
+ int _tmain(int argc, _TCHAR* argv[])
 {
 	setlocale(LC_ALL,"RUS");
 
 	//Первая DLL
 	cout << "Вывод группы: \nПервая функция: вывод номера группы в формате int: " << OutGroupInt(571227) << "\n";
 	cout << "Вторая функция: вывод номера группы в формате char: ";
-    OutGroupChar();
+	OutGroupChar();
 	cout << "\n\n";
 
 	//Вторая DLL
-	wchar_t * str = new wchar_t [25];
-	str = OutConsole();
+	wchar_t * str;
+	str = ConsoleOut();
 	cout << "Вывод ФИО: \nПервая функция: вывод ФИО в консоль: ";
 	cout << OutConsole();
 	cout << "\n";
